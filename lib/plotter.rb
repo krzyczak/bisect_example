d1105f4a25325ad9caaf9688373c189e7903acb8
krzyczak <krzyczak@users.noreply.github.com>
2019-06-03 13:16:10 +0100

    Remove code smell - 2

--- lib/plotter.rb
+++ lib/plotter.rb
@@ -7,9 +7,15 @@ class Plotter
     puts "Starting to plot images."
     @data.map do |dat|
       puts "--- Currently plotting: #{dat}"
-      content = "sdfsdfdssdfkhsdfkjsdhf-#{File.read("./ground_truth_plot.png")}"
-      File.open("./results/#{dat}.png", "w+") { |ff| ff.write(content) }
+      create_plot(dat)
     end
     puts "Finished plotting all plots."
+  end
+
+  private
+
+  def create_plot(dat)
+    content = "sdfsdfdssdfkhsdfkjsdhf-#{File.read("./ground_truth_plot.png")}"
+    File.open("./results/#{dat}.png", "w+") { |ff| ff.write(content) }
   end
 end
